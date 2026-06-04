12367614f7077c32c28907f1a2e369e6957a97bf
Leoš Petzold <petzoldleos@gmail.com>
2026-06-04 17:07:11 +0200

    Workstation commit

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,10 +1,15 @@
 const tabButtons = $(".tabb");
 const tabs = $(".tab");
 Array.from(tabButtons).forEach(button => {
-    button.addEventListener("click", () => {
+    button.addEventListener("mousedown", () => {
         Array.from(tabs).forEach(tab => {
             tab.style.display = "none";
         });
         document.getElementById(button.getAttribute("value")).style.display = "unset";
+
+        Array.from(tabButtons).forEach(btn => {
+            btn.setAttribute("active", false);
+        });
+        button.setAttribute("active", true);
     });
 });
